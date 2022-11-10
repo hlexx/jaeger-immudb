289f2f00dd3e40c4d8d691b1d4529b17a6c1e014
deemak <deema_k@mail.ru>
2022-11-10 15:17:47 +0300

    add all errors log

--- cmd/badger-exporter/exporter.go
+++ cmd/badger-exporter/exporter.go
@@ -33,54 +33,59 @@ func main() {
 	}
 	logger.Warn("Init immudb config file")
 	for {
-		logger.Warn("init tempDir")
-		file, err := ioutil.TempDir(os.TempDir(), "bdata")
-		if err != nil {
-			logger.Error(fmt.Sprintf("failed create tempfile %v", err.Error()))
-			time.Sleep(sleepTime)
-			continue
-		}
-		logger.Warn("copy badger dir")
-		err = utils.CopyDir(originalPath, file)
-		if err != nil {
-			logger.Error(fmt.Sprintf("failed to copy dir %v", err.Error()))
-			time.Sleep(sleepTime)
-			err := os.RemoveAll(file)
+		select {
+		case <-time.After(time.Second * 30):
+			logger.Warn("service work")
+		default:
+			logger.Warn("init tempDir")
+			file, err := ioutil.TempDir(os.TempDir(), "bdata")
+			if err != nil {
+				logger.Error(fmt.Sprintf("failed create tempfile %v", err.Error()))
+				time.Sleep(sleepTime)
+				continue
+			}
+			logger.Warn("copy badger dir")
+			err = utils.CopyDir(originalPath, file)
+			if err != nil {
+				logger.Error(fmt.Sprintf("failed to copy dir %v", err.Error()))
+				time.Sleep(sleepTime)
+				err := os.RemoveAll(file)
+				if err != nil {
+					logger.Error(fmt.Sprintf("path %v remove all %v", file, err.Error()))
+				}
+				continue
+			}
+			path := fmt.Sprintf("%s/key", file)
+			logger.Warn("export data  to immudb")
+			opts := badgerV3.DefaultOptions(path)
+			opts.SyncWrites = false
+			opts.ValueDir = fmt.Sprintf("%s/value", file)
+			store, err := badgerV3.Open(opts)
+			if err != nil {
+				logger.Error(fmt.Sprintf("failed badger open %v", err.Error()))
+				time.Sleep(sleepTime)
+				err := os.RemoveAll(file)
+				if err != nil {
+					logger.Error(fmt.Sprintf("path %v remove all %v", file, err.Error()))
+				}
+				continue
+			}
+			err = driver.ImportFromBackup(store)
+			if err != nil {
+				logger.Error(fmt.Sprintf("failed import from backup %v", err.Error()))
+				time.Sleep(sleepTime)
+				err := os.RemoveAll(file)
+				if err != nil {
+					logger.Error(fmt.Sprintf("path %v remove all %v", file, err.Error()))
+				}
+				continue
+			}
+			err = os.RemoveAll(file)
 			if err != nil {
 				logger.Error(fmt.Sprintf("path %v remove all %v", file, err.Error()))
 			}
-			continue
-		}
-		path := fmt.Sprintf("%s/key", file)
-		logger.Warn("export data  to immudb")
-		opts := badgerV3.DefaultOptions(path)
-		opts.SyncWrites = false
-		opts.ValueDir = fmt.Sprintf("%s/value", file)
-		store, err := badgerV3.Open(opts)
-		if err != nil {
-			logger.Error(fmt.Sprintf("failed badger open %v", err.Error()))
+			logger.Warn("remove dir")
 			time.Sleep(sleepTime)
-			err := os.RemoveAll(file)
-			if err != nil {
-				logger.Error(fmt.Sprintf("path %v remove all %v", file, err.Error()))
-			}
-			continue
 		}
-		err = driver.ImportFromBackup(store)
-		if err != nil {
-			logger.Error(fmt.Sprintf("failed import from backup %v", err.Error()))
-			time.Sleep(sleepTime)
-			err := os.RemoveAll(file)
-			if err != nil {
-				logger.Error(fmt.Sprintf("path %v remove all %v", file, err.Error()))
-			}
-			continue
-		}
-		err = os.RemoveAll(file)
-		if err != nil {
-			logger.Error(fmt.Sprintf("path %v remove all %v", file, err.Error()))
-		}
-		logger.Warn("remove dir")
-		time.Sleep(sleepTime)
 	}
 }
